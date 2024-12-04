845be6f1b995f41d0ebf1f2a808f14298ae34fc2
lnoemeyer <lukas.noemeyer@gmail.com>
2024-12-04 12:48:56 +0100

    fix: changed submission object

--- src/main/java/at/jku/dke/task_app/sql_ddl/controllers/SubmissionController.java
+++ src/main/java/at/jku/dke/task_app/sql_ddl/controllers/SubmissionController.java
@@ -1,13 +1,13 @@
 package at.jku.dke.task_app.sql_ddl.controllers;
 
 import at.jku.dke.etutor.task_app.controllers.BaseSubmissionController;
-import at.jku.dke.task_app.sql_ddl.data.entities.SqlDdlSubmission;
+import at.jku.dke.task_app.sql_ddl.data.entities.Submission;
 import at.jku.dke.task_app.sql_ddl.dto.SqlDdlSubmissionDto;
 import at.jku.dke.task_app.sql_ddl.services.SqlDdlSubmissionService;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Controller for managing {@link SqlDdlSubmission}s.
+ * Controller for managing {@link Submission}s.
  */
 @RestController
 public class SubmissionController extends BaseSubmissionController<SqlDdlSubmissionDto> {
--- src/main/java/at/jku/dke/task_app/sql_ddl/data/repositories/SqlDdlSubmissionRepository.java
+++ src/main/java/at/jku/dke/task_app/sql_ddl/data/repositories/SqlDdlSubmissionRepository.java
@@ -1,11 +1,11 @@
 package at.jku.dke.task_app.sql_ddl.data.repositories;
 
 import at.jku.dke.etutor.task_app.data.repositories.SubmissionRepository;
-import at.jku.dke.task_app.sql_ddl.data.entities.SqlDdlSubmission;
+import at.jku.dke.task_app.sql_ddl.data.entities.Submission;
 
 /**
  * The interface Sql ddl submission repository.
  */
-public interface SqlDdlSubmissionRepository extends SubmissionRepository<SqlDdlSubmission> {
+public interface SqlDdlSubmissionRepository extends SubmissionRepository<Submission> {
 
 }
